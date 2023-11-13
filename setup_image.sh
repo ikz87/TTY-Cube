d9e9781d217ecf5197fbff4340bc17f90baa7a94
ikz87 <iikz87ii@gmail.com>
2023-11-13 14:48:19 -0600

    Fix

--- setup_image.sh
+++ setup_image.sh
@@ -10,3 +10,5 @@ side_length=`grep "#define SIDE_LENGTH" "$script_dir/config.h" | awk '{printf $3
 convert "$image" -resize $side_length\x$side_length\! "$script_dir/image.png"
 
 python "$script_dir/image_to_binary_data.py" "$script_dir/image.png"
+
+echo "Done. Ensure that you uncommented the IMAGE definition line in config.h"
